b95c0e70d145f2c557d5220018b9831d8b3a70f5
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-07-31 11:43:48 +0530

    Store Password using bcrypt

--- src/index.js
+++ src/index.js
@@ -15,3 +15,19 @@ app.use(taskRouter)
 app.listen(port, () => {
 	console.log('Server is up and running on port ' + port)
 })
+
+
+const bcrypt = require('bcrypt')
+
+const myFunction = async () => {
+	const password = 'Red12345!'
+	const hashedPassword = await bcrypt.hash(password, 8)
+
+	console.log(password)
+	console.log(hashedPassword)
+
+	const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+	console.log(isMatch)
+}
+
+myFunction()
