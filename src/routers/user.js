df591df8581eeb8fdaeb91031f8caa5d96b1d9ed
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-08-01 21:58:00 +0530

    Added Login Functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -11,7 +11,15 @@ router.post('/users', async (req, res) => {
 	} catch (e) {
 		res.status(400).send(e)
 	}
+})
 
+router.post('/users/login', async (req, res) => {
+	try {
+		const user = await User.findByCredentials(req.body.email, req.body.password)
+		res.send(user)
+	} catch (e) {
+		res.status(400).send()
+	}
 })
 
 router.get('/users', async (req, res) => {
@@ -20,7 +28,7 @@ router.get('/users', async (req, res) => {
 		const users = await User.find({})
 		res.send(users)
 	} catch (e) {
-		res.status(500).send()
+		res.status(500).send(e)
 	}
 })
 
