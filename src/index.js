477ebbe99fc6c6953009f63875f625e2662d9733
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-08-05 00:34:25 +0530

    Endpoints Authentication

--- src/index.js
+++ src/index.js
@@ -27,16 +27,3 @@ app.use(taskRouter)
 app.listen(port, () => {
 	console.log('Server is up and running on port ' + port)
 })
-
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-	const token = jwt.sign({ _id: 'abc123' }, 'thisismynodeapp', { expiresIn: '7 days' })
-	console.log(token)
-
-	const data = jwt.verify(token, 'thisismynodeapp')
-	console.log(data)
-}
-
-myFunction()
