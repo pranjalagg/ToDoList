d4fe4f102a07ab0d177804aa3d60e6b35f4c06cf
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-08-04 20:09:50 +0530

    Added Authentication

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+// 	if (req.method === 'GET') {
+// 		res.send('GET requests are disabled')
+// 	} else {
+// 		next()
+// 	}
+// })
+
+// app.use((req, res, next) => {
+// 	res.status(503).send('The site is under maintainance, please try after sometime')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -17,17 +29,14 @@ app.listen(port, () => {
 })
 
 
-const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-	const password = 'Red12345!'
-	const hashedPassword = await bcrypt.hash(password, 8)
+	const token = jwt.sign({ _id: 'abc123' }, 'thisismynodeapp', { expiresIn: '7 days' })
+	console.log(token)
 
-	console.log(password)
-	console.log(hashedPassword)
-
-	const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-	console.log(isMatch)
+	const data = jwt.verify(token, 'thisismynodeapp')
+	console.log(data)
 }
 
 myFunction()
