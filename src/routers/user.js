ac6dad81cb0f5ab649e145e2e49e59e8dff3fcc6
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-08-12 19:01:38 +0530

    Error Handling

--- src/routers/user.js
+++ src/routers/user.js
@@ -91,10 +91,21 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-	dest: 'avatars'
+	dest: 'avatars',
+	limits: {
+		fileSize: 1000000
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+			return cb(new Error('This format is not supported!'))
+		}
+		cb(undefined, true)
+	}
 })
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
 	res.send()
+}, (error, req, res, next) => {
+	res.status(400).send({ error: error.message })
 })
 
 module.exports = router
