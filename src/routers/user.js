7ea27733ce3f3c2fbdf4a5db5bf387a30dcee46d
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-08-16 00:19:10 +0530

    Profile Pic Feature Added

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
-const multer = require('multer')
 
 router.post('/users', async (req, res) => {
 	const user = new User(req.body)
@@ -91,7 +92,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-	dest: 'avatars',
 	limits: {
 		fileSize: 1000000
 	},
@@ -102,10 +102,34 @@ const upload = multer({
 		cb(undefined, true)
 	}
 })
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+	const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+	req.user.avatar = buffer
+	await req.user.save()
 	res.send()
 }, (error, req, res, next) => {
 	res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+	req.user.avatar = undefined
+	await req.user.save()
+	res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+		if (!user || !user.avatar) {
+			throw new Error()
+		}
+
+		res.set('Content-Type', 'image/png')
+		res.send(user.avatar)
+	} catch (e) {
+		res.status(404).send()
+	}
 })
 
 module.exports = router
