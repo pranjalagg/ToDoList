1c2e9c917ea5899404b0a7d3b0468321ce4c1abc
Pranjal Aggarwal <pranjalaggarwal1999@gmail.com>
2020-08-16 02:31:01 +0530

    Setting up ENV Variables

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
 	sgMail.send({
 		to: email,
-		from: 'andrew@mead.io',
+		from: 'pranjalaggarwal1999@gmail.com',
 		subject: 'Thanks for joining in!',
 		text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
 	})
